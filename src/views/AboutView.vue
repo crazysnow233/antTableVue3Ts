477c12c2c4c7d3503b06338faed9b581e03c6309
haohaohao <hao@haohaohaodeMBP.lan>
2022-07-07 21:56:51 +0800

    创建项目

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a-button>你好</a-button>
   </div>
 </template>
+<script lang="ts" setup>
+import { Modal } from "ant-design-vue";
+Modal.error({
+  content: "你哈",
+});
+</script>
